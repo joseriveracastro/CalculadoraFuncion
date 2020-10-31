edb81c5bbde5ebea237d05f2895e28c485d8a1c6
Jose Rivera <jriverac@gmail.com>
2020-10-31 14:21:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def calculadora(a=1,b=1,c=1):
+  if c == 1:
+    suma = a+b
+    print('La suma de los numeros es',suma)
+  elif c == 2:
+    resta = a-b
+    print('La resta de los numeros es',resta)
+  else:
+    print('Opcion invalida')
+
+
+print('Calculadora basica')
+numero1 = int(input('Digite un numero '))
+numero2 = int(input('Digite otro numero '))
+opcion = int(input('Digite (1) para suma y (2) para resta '))
+
+calculadora(numero1,numero2,opcion)
